b2a231b6da978911dab95ff4823bf4aa3d6ea6c5
Nathan Barnes <339723215@GoTVDSB.ca>
2024-11-28 09:39:49 -0500

    ffdsubdsfuhbdhfb

--- Summative animation project/Game1.cs
+++ Summative animation project/Game1.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Summative_animation_project
 {
+    enum Screen
+    {
+        Intro,
+        play,
+        touchDown
+    }
+    
+
+    
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        MouseState mouseState;
         Texture2D footallField;
-        Texture2D football;
+        Texture2D huddle;
+        Rectangle window;
+
+
+        
+        Screen screen;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -21,17 +37,20 @@ namespace Summative_animation_project
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            _graphics.PreferredBackBufferWidth = 600;
-            _graphics.PreferredBackBufferHeight = 600;
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
             base.Initialize();
+            screen = Screen.Intro;
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             footallField = Content.Load<Texture2D>("footballfield");
-            football = Content.Load<Texture2D>("footballl");
+            huddle = Content.Load<Texture2D>("huddle");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -40,7 +59,12 @@ namespace Summative_animation_project
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.play;
+            }
+            
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -50,8 +74,16 @@ namespace Summative_animation_project
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            _spriteBatch.Draw(footallField, new Rectangle(0, 0, 600, 600), Color.White);
-            _spriteBatch.Draw(football, new Rectangle(0, 0, 50, 50), Color.White);
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(huddle, window, Color.White);
+            }
+            else if (screen == Screen.play)
+            {
+                _spriteBatch.Draw(footallField, new Rectangle(0, 0, 600, 600), Color.White);
+            }
+            
+            
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
