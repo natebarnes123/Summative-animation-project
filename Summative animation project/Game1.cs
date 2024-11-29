8fc03fdf03571ea072136bbb530708324ec512e8
Nathan Barnes <339723215@GoTVDSB.ca>
2024-11-29 09:39:55 -0500

    working

--- Summative animation project/Game1.cs
+++ Summative animation project/Game1.cs
@@ -23,9 +23,10 @@ namespace Summative_animation_project
         Texture2D footallField;
         Texture2D huddle;
         Rectangle window;
-
-
-        
+        Texture2D playerRunning;
+        Rectangle playerRunningRect;
+        Texture2D tacklingPlayer;
+        Vector2 playerSpeed;
         Screen screen;
         public Game1()
         {
@@ -38,6 +39,8 @@ namespace Summative_animation_project
         {
             // TODO: Add your initialization logic here
             window = new Rectangle(0, 0, 800, 600);
+            playerSpeed = new Vector2(2, 0);
+            playerRunningRect = new Rectangle(84, 320, 150, 150);
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
@@ -50,13 +53,15 @@ namespace Summative_animation_project
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             footallField = Content.Load<Texture2D>("footballfield");
             huddle = Content.Load<Texture2D>("huddle");
-
-
+            playerRunning = Content.Load <Texture2D>("playerRunning");
+            tacklingPlayer = Content.Load<Texture2D>("tacklingPlayer");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+            this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}";
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             if (screen == Screen.Intro)
@@ -64,7 +69,26 @@ namespace Summative_animation_project
                 if (mouseState.LeftButton == ButtonState.Pressed)
                     screen = Screen.play;
             }
-            
+            else if (screen == Screen.play)
+            {
+                playerRunningRect.X += (int)playerSpeed.X;
+                playerRunningRect.Y += (int)playerSpeed.Y;
+
+                if (playerRunningRect.Right > window.Width)
+                    playerSpeed.X *= -1;
+                if (playerRunningRect.Right > window.Width)
+                {
+                    screen = Screen.touchDown;
+                }
+                if(playerRunningRect.X>325)
+                {
+                    playerSpeed.Y = -2;
+                }
+                if(playerRunningRect.Y>200)
+                {
+                    playerSpeed.Y = 0;
+                }
+            }
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -80,8 +104,11 @@ namespace Summative_animation_project
             }
             else if (screen == Screen.play)
             {
-                _spriteBatch.Draw(footallField, new Rectangle(0, 0, 600, 600), Color.White);
+                _spriteBatch.Draw(footallField, window, Color.White);
+                _spriteBatch.Draw(playerRunning, playerRunningRect, Color.White);
+                _spriteBatch.Draw(tacklingPlayer, new Rectangle(450, 330, 150, 150), Color.White);
             }
+            
             
             
             _spriteBatch.End();
